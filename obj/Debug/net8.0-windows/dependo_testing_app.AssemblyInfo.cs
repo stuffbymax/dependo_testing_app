aa933bf6073ad69720367a96feb2e520696a1d3f
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2025-08-03 17:53:26 +0100

    Add game dependency data to runtimeconfig.json
    Expanded dependo_testing_app.runtimeconfig.json to include detailed dependency and troubleshooting information for multiple popular games. This data includes required DirectX, Visual C++ Redistributables, .NET versions, DLLs, download links, and common fixes, enabling the app to provide targeted support for game dependency issues.

--- obj/Debug/net8.0-windows/dependo_testing_app.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/dependo_testing_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dependo_testing_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4dcfc6911b0ae42060495173e7acf4380346ab7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8d9c43fe9ba0c0e831b02ab5d63fcae1c4ec31")]
 [assembly: System.Reflection.AssemblyProductAttribute("dependo_testing_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dependo_testing_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
